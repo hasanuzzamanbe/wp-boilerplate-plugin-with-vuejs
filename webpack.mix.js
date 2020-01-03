0cb8f8b120e937f53b9607099684a75a05aa5095
Hasanuzzaman <hasanuzzamanbe@gmail.com>
2020-01-03 12:09:56 +0600

    dirrectory issue solved

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,6 @@ let mix = require('laravel-mix');
 
 mix.setResourceRoot('../');
 mix
-    .js('src/js/Boot.js', 'assets/js/boot.js')
+    .js('src/js/boot.js', 'assets/js/boot.js')
     .js('src/js/main.js', 'assets/js/plugin-name.js')
 
