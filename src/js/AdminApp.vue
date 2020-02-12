2b7e19bf46a63e1cf607170de9a6534a41d475c6
Hasanuzzaman <hasanuzzamanbe@gmail.com>
2020-02-12 11:09:36 +0600

    Global scss file added

--- src/js/AdminApp.vue
+++ src/js/AdminApp.vue
@@ -43,36 +43,3 @@
       }
   }
 </script>
-<style>
-    .topnav {
-        background-color: #fff;
-        overflow: hidden;
-        border-top: 1px solid #32373c2b;
-        border-bottom: 1px solid #32373c2b;
-        width: 100%;
-        /* margin-left: -20px; */
-    }
-    .update-nag {
-        display: none;
-    }
-    /* Style the links inside the navigation bar */
-    .topnav a {
-        float: left;
-        color: black;
-        text-align: center;
-        padding: 14px 16px;
-        text-decoration: none;
-        font-size: 17px;
-    }
-    /* Change the color of links on hover */
-    .topnav a:hover {
-        background-color: #ddd;
-        color: black;
-    }
-    /* Add a color to the active/current link */
-    .topnav a.ninja-tab-active {
-        color: #ffa92c;
-        border-bottom: 2px solid #ffa92c;
-    }
-
-</style>
